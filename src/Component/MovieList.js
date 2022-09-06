d6d2946bbcd2e54343fdd0dcaa47a569ed80c7b3
hajerabidli <106708236+hajerabidli@users.noreply.github.com>
2022-09-06 19:43:00 +0200

    first commit

---
+++ src/Component/MovieList.js
@@ -0,0 +1,20 @@
+import MovieCard from "./MovieCard";
+
+function MovieList({ movies, searchTitle, searchRate }) {
+  return (
+    <div className="row justify-content-center">
+      {movies
+        .filter(
+          (movie) =>
+            movie.title
+              .toUpperCase()
+              .includes(searchTitle.toUpperCase().trim()) &&
+            movie.rate >= searchRate
+        )
+        .map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+    </div>
+  );
+}
+export default MovieList;
---
+++ src/Component/NavBar.js
@@ -0,0 +1,37 @@
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+import ReactStars from "react-rating-stars-component";
+
+function NavBar({ setsearchTitle, setsearchRate }) {
+  const ratingChanged = (newRating) => {
+    setsearchRate(newRating);
+  };
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <ReactStars
+          count={5}
+          onChange={ratingChanged}
+          size={24}
+          activeColor="#ffd700"
+        />
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+            onChange={(e) => setsearchTitle(e.target.value)}
+          />
+          <Button variant="outline-secondary">Search</Button>
+        </Form>
+      </Container>
+    </Navbar>
+  );
+}
+export default NavBar;
